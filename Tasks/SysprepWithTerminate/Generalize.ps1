f95476d442b6b707c98340bb2e341d7c0d55784b
WangJianMS <100215923+wngjian4814@users.noreply.github.com>
2024-02-12 10:26:14 -0800

    Update Generalize.ps1

--- Tasks/SysprepWithTerminate/Generalize.ps1
+++ Tasks/SysprepWithTerminate/Generalize.ps1
@@ -1,6 +1,6 @@
 $newProcess = new-object System.Diagnostics.ProcessStartInfo "sysprep.exe" 
 $newProcess.WorkingDirectory = "${env:SystemDrive}\windows\system32\sysprep" 
-$newProcess.Arguments = "/sysprep /oobe /generalize /quit" 
+$newProcess.Arguments = "/generalize /oobe /quit" 
 Write-Host $newProcess.Arguments 
 $newProcess.Verb = "runas"; 
 $process = [System.Diagnostics.Process]::Start($newProcess);
